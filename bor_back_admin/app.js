cc89a4fe49f7a6a3bbea2c779cf260c76e2ff38c
kokalibi <balint00istvan@gmail.com>
2025-11-06 12:34:02 +0100

    :P

--- bor_back_admin/app.js
+++ bor_back_admin/app.js
@@ -2,8 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config();
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,13 +15,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 const cors = require('cors');
-
-
 var corsOptions={
     "credentials" : true,
     origin: "http://localhost:3000"
 }
 app.use(cors(corsOptions));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/borok', borokRouter);
--- bor_back_admin/routes/borok.js
+++ bor_back_admin/routes/borok.js
@@ -2,3 +2,4 @@ var Express = require('express');
 var router = Express.Router();
 const borokController = require('../controllers/bor_controller');
 router.get('/', borokController.getAll);
+module.exports = router;
